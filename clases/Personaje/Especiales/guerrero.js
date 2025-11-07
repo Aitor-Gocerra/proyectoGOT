c24d380e0d43d4094adcd9e429a651b74c50eb5d
Aitor-Gocerra <aitorgomezcerrato.guadalupe@alumnado.fundacionloyola.net>
2025-11-07 19:46:02 +0100

    Add battle logic and enhance Guerrero class
    Introduced the Batalla class to handle battles between houses, tracking fallen warriors and determining the winner. Enhanced the Guerrero class with health management, attack, and damage reception methods to support battle mechanics.

--- clases/Personaje/Especiales/guerrero.js
+++ clases/Personaje/Especiales/guerrero.js
@@ -3,13 +3,14 @@ import { Arma } from "../../arma";
 
 export class Guerrero extends Personaje{
     #arma;
+    #vida = 100;
 
     constructor(nombre = "", age = 0, vivo = true, casa = "", arma = null){
         super(nombre, age, vivo, casa);
 
         if(arma instanceof Arma) {
             this.#arma = arma;
-        } 
+        }
         
     }
 
@@ -37,5 +38,23 @@ export class Guerrero extends Personaje{
 
     entrenar(){
         console.log(`${this.nombre} se entrena sin descanso para la proxima batalla.`);   
+    }
+
+    morir(){
+        console.log(`${this.nombre} ha muerto.`);
+        
+    }
+    recibirDaño(puntos){
+        this.#vida -= puntos;
+        if(this.#vida <= 0){
+            this.#vida = 0;
+            this.morir();
+        }
+    }
+
+    atacar(objetivo){
+        const daño = Math.floor(this.#arma.daño * (0.7 + Math.random() * 0.6));
+        console.log(`${this.nombre} ataca a ${objetivo.nombre} causando ${daño} puntos de vida.`);
+        objetivo.recibirDaño(daño);
     }
 }
---
+++ clases/batalla.js
@@ -0,0 +1,54 @@
+import { Casa } from "./casa.js";
+import { Guerrero } from "./Personaje/Especiales/guerrero.js";
+
+export class Batalla {
+    #guerrerosMuertos = [];
+
+    iniciarBatalla (casaA, casaB){
+
+        const guerrerosCasaA = [];
+        const guerrerosCasaB = [];
+
+        if(casaA instanceof Casa){
+            guerrerosCasaA = casaA.miembros.filter(miembro => miembro instanceof Guerrero);
+        }
+        
+        if(casaB instanceof Casa){
+            guerrerosCasaA = casaB.miembros.filter(miembro => miembro instanceof Guerrero);
+        }
+
+        while(guerrerosCasaA.length > 0 && guerrerosCasaB.length > 0){
+            
+            const guerreroA = guerrerosCasaA[0];
+            const guerreroB = guerrerosCasaB[0];
+
+            guerreroA.ataca(guerreroB);
+            guerreroB.ataca(guerreroA);
+
+            if (guerreroA.vida <= 0) {
+                this.#guerrerosMuertos.push(guerreroA);
+                console.log(`${guerreroA.nombre} ha muerto`);
+                guerrerosCasaA.shift(); 
+            }
+
+            if (guerreroB.vida <= 0) {
+                this.#guerrerosMuertos.push(guerreroB);
+                console.log(`${guerreroB.nombre} ha muerto`);
+                guerrerosCasaB.shift(); 
+            }
+        }
+
+        if (guerrerosCasaA.length > 0) {
+            console.log(`La casa ${casaA.nombre} gana la batalla.`);
+        } else if (guerrerosCasaB.length > 0) {
+            console.log(`La casa ${casaB.nombre} gana la batalla.`);
+        } else {
+            console.log("¡Ambas casas han caído en combate!");
+        }
+    }
+
+    mostrarGuerrerosMuertosEnCombate(){
+        console.log("Guerreros que han muerto en combate:");
+        this.#guerrerosMuertos.forEach(guerrero => console.log(`- ${guerrero.nombre}`));
+    }
+}
